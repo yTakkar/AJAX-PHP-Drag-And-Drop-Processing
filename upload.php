cc4b856863dba4384f21c7a7abac231fd942c4f1
yTakkar <www.shtakkar@gmail.com>
2017-09-11 20:08:58 +0530

    Some code changes

--- upload.php
+++ upload.php
@@ -1,18 +1,25 @@
 <?php
-if(is_array($_FILES))
-{
- if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
-  $sourcePath = $_FILES['userImage']['tmp_name'];
-  $targetPath = "images/".$_FILES['userImage']['name'];
-  if(move_uploaded_file($sourcePath,$targetPath)) {
-  ?>
-  <div class="output">
-    <img src="<?php echo $targetPath; ?>">
-    <span>uploaded</span>
-  </div>
-   <?php
-   exit();
+
+  $files = $_FILES["userImage"];
+
+  if($files) {
+    if(is_uploaded_file($files['tmp_name'])) {
+
+      $sourcePath = $files['tmp_name'];
+      $targetPath = "images/".$files['name'];
+
+      if(move_uploaded_file($sourcePath, $targetPath)) {
+
+?>
+
+        <div class="output">
+          <img src="<?php echo $targetPath; ?>">
+          <span>uploaded</span>
+        </div>
+
+<?php
+      exit();
+      }
+    }
   }
- }
-}
 ?>
